ef530b6887ff9352798cda769223fcc8c530a0bb
Francis Uloko <francisuloko@yahoo.com>
2021-09-13 23:28:11 +0100

    Setup redux store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducer, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import covidReducer from './covid/covid';
+
+const reducer = combineReducer({
+  covid: covidReducer,
+});
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger, thunk),
+);
+
+export default store;
