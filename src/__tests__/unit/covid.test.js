3acf441c35af976a98424a12f5769edd39fac289
Francis Uloko <francisuloko@yahoo.com>
2021-09-18 17:58:15 +0100

    Add more negative test cases

--- src/__tests__/unit/covid.test.js
+++ src/__tests__/unit/covid.test.js
@@ -18,7 +18,15 @@ describe('Covid Reducer', () => {
     expect(CovidReducer({}, ({ type: GET_DATA_SUCCESS, data }))).toStrictEqual(response);
   });
 
+  it('store data in redux not to be undefined', () => {
+    expect(CovidReducer({}, ({ type: GET_DATA_SUCCESS, data }))).not.toBeUndefined();
+  });
+
   it('Filter Country Action', () => {
     expect(CovidReducer({}, (filterCountry('Afghanistan')))).toStrictEqual({ country: 'Afghanistan' });
+  });
+
+  it('Filter Country Action not to be undefined', () => {
+    expect(CovidReducer({}, (filterCountry('Afghanistan')))).not.toBeUndefined();
   });
 });
